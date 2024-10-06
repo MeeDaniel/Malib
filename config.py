ac8a7313ee1a75890302641d90ff263cede37189
MeeDaniel <danya.cnh@gmial.com>
2024-10-06 16:26:53 +0300

    first commit

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ from datetime import timedelta
 
 class Config:
     SECRET_KEY = "YOU_WILL_NEVER_GUESS"
-    MONGO_URI = "mongodb+srv://vohoa:vohoa@cluster0.deqp0qk.mongodb.net/vohoa"
+    MONGO_URI = "..."
     PERMANENT_SESSION_LIFETIME = timedelta(weeks=5)
